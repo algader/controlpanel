6e424e9f137dc5ed1b242cd839f216f2ea4b6f2d
muhammad abd algader alsukkari <alsukkari.mohammad@icloud.com>
2021-12-19 11:17:40 +0100

    merging

---
+++ Desktop/control-panel/webpack.config.js
@@ -0,0 +1,76 @@
+var path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+module.exports = {
+    entry:  {
+      'app': './src/index.js',
+    },
+    output: {
+      publicPath: '/',
+        path: path.join(__dirname, "/app"),
+        filename: 'app.js',
+      },
+      //devServer: {
+        devServer: {
+       // contentBase: path.join(__dirname, "/app"),
+       static: path.join(__dirname, '/app'),
+        port: 8081,
+       // writeToDisk: true,
+       devMiddleware: {
+        writeToDisk: true,
+      },
+      },
+      
+      module: {
+        rules: [
+          {
+            test: /\.html$/,
+            use: [
+              {
+                loader: "html-loader",
+              }
+            ]
+          },
+
+          {
+            test: /\.(sa|sc|c)ss$/,
+            use: [
+              MiniCssExtractPlugin.loader, 
+              'css-loader', 
+              'postcss-loader',
+              'sass-loader'
+            ]
+          },
+          {
+            test: /\.(svg|eot|woff|woff2|ttf)$/,
+            exclude: /images/,
+            use: [
+              {
+                loader: "file-loader", 
+                options: {
+                  name: '[name].[ext]',
+                  outputPath: "assets/fonts",
+                }
+              }
+            ]
+          },
+          
+        ]
+      },
+
+  
+    
+      plugins: [
+        new CleanWebpackPlugin({cleanStalewebpackAssets: false}),
+        new OptimizeCSSAssetsPlugin({}),
+        new MiniCssExtractPlugin({
+          filename: "assets/css/styles.css"
+        }),
+        new HtmlWebpackPlugin({ 
+            filename: "index.html",
+            template: "./src/index.html",
+        })
+      ]   
+}   
